b7997f78b08c0163901c71c52e37b86d8f91ebb6
Aprendiz <Aprendiz@BOGDFPCGMF4147.FormacionSena.RED>
2024-10-11 11:26:06 -0500

    feat: nuevo apartados

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,52 @@
-import {React , useState} from 'react'
+import { React , useState } from 'react'
 import Header from './components/Header'
 import ComentarioItem from './components/ComentarioItem';
 import comentarios from './data/Comentarios';
+import ComentarioLista from './components/ComentarioLista';
+import Card from './components/Card';
+import ComentarioStats from './components/ComentarioStats';
+import ComentarioForm from './components/ComentarioForm';
 
 function App() {
 
-    const [comments ,
-         setComments] = useState(comentarios)
+    const [comments 
+        , setComments] 
+        = useState(comentarios)
+
+    const borrarItem=id=>  {
+          if (window.confirm
+            ('Esta seguro de borrar el comentario?')) {
+          //asignar: nuevo estado a comments:
+          //filter: para quitar los comentarios
+          //cuyo id concuerde con el parametro
+          setComments (comments.filter((c) => c.id !== id ))
+            }
+          }
+          
 
     const titulo = "App de Comentarios";
     const Autor = "Eric Molano"
     const ficha = "2902093"
-    const centrodeformacion = "Centro de Gestion de Mercados, Logistica y Tecnologias de la informacion"
+    const centrodeformacion = "CGMLTI"
 
    
     const loading = false;
-    const showComments = true;
     if (loading === true) return (<h1> Cargando Comentarios...</h1>)
 
   return (
     <div className='container'>
 
-        <Header titulo={ titulo } autor={Autor} ficha={ficha} centrodeformacion={centrodeformacion} />
+        <Header 
+        titulo={ titulo } 
+        autor={Autor} 
+        ficha={ficha} 
+        centrodeformacion={centrodeformacion} />
+        <ComentarioForm/>
+        <ComentarioStats comentarios={comments} />
+        <ComentarioLista comments={comments}
+                        handleDelete={borrarItem} />
 
-        { showComments &&
-    (
-
-        <div className='comments'>
-        <h3> Comentarios: {comentarios.length} </h3>
-        <ul>
-            {
-                comments.map(comentario => 
-                    <ComentarioItem key={comentario.id}
-                    comentario={comentario.comentario}
-                    calificacion={comentario.calificacion}
-                     />
-            )
-            }
-        </ul>
     </div>
-    )
-
-  }
-    
-</div>
   )
 
 }
